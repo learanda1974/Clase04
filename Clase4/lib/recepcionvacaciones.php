e329b99fc78f0f23d160b5a1c78a39327e0109ed
cetecom <cetecom@LCV10807>
2019-03-18 22:32:04 -0300

    Clase 18-03-2019

---
+++ Clase4/lib/recepcionvacaciones.php
@@ -0,0 +1,20 @@
+<?php
+include('constantes.php');
+include('vacaciones.php');
+
+$oVacaciones=new Vacaciones($_POST["rut"], $_POST["nombre"], $_POST["cargo"], "", "", "");
+if(isset($_SESSION["aVacaciones"])){
+    $arrVacaciones=$_SESSION["aVacaciones"];    
+}
+$arrVacaciones[]=$oVacaciones;
+$_SESSION["oVacaciones"]=$arrVacaciones;    
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
